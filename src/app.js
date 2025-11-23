1904108d34d17133697a2ae0bab77893388a32bf
Miguelangel <mig2k12@outlook.es>
2025-11-23 15:09:21 +0000

    proyecto carta

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  
+  let indicePalo = Math.floor(Math.random() * palos.length);
+  let indiceNumero = Math.floor(Math.random() * numeros.length);
+
+  
+  let paloFinal = palos[indicePalo];
+  let numeroFinal = numeros[indiceNumero];
+
+  
+  let topElement = document.querySelector("#top-suit");
+  let numberElement = document.querySelector("#card-value");
+  let bottomElement = document.querySelector("#bottom-suit");
+  let carta = document.querySelector("#cartaPoker")
+  
+  let colorClass = (paloFinal === "♥" || paloFinal === "♦") ? "text-danger" : "text-dark";
+
+  
+  topElement.innerHTML = paloFinal;
+  numberElement.innerHTML = numeroFinal;
+  bottomElement.innerHTML = paloFinal;
+
+  
+  topElement.className = `fs-1 text-start ${colorClass}`;
+  numberElement.classList.add(colorClass); 
+  bottomElement.className = `fs-1 text-start ${colorClass}`;
+
+  carta.classList.remove("anim-girar");
+  
+  void carta.offsetWidth;
+  
+  carta.classList.add("anim-girar");
 };
